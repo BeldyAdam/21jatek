25444a965e186ad1aaf899d2aaaa3073be174409
BeldyAdam <beldy.adam@gmail.com>
2023-02-02 16:47:57 +0100

    folyamatabra

--- main.py
+++ main.py
@@ -37,3 +37,75 @@ def jatekos_tobb_21(allapot):
 def jatekos_kevesebb_gep():
     jatekos_vesztett()
 
+
+def jatekos_vesztett_21_nagyobb_teszt():
+    j_lapok = [10, 9, 3]
+    g_lapok = [10, 9]
+    vart_eredmeny = "A játékos vesztett!"
+    kapott_eredmeny = eredmeny(j_lapok, g_lapok)
+    if vart_eredmeny == kapott_eredmeny:
+        print("A játékos 21-nél több, a teszt sikeres")
+    else:
+        print("A játékos 21-nél több, a teszt megbukott")
+
+
+def jatekos_vesztett_21_kevesebb_teszt():
+    j_lapok = [10, 9, 1]
+    g_lapok = [10, 9, 2]
+    vart_eredmeny = "A játékos vesztett!"
+    kapott_eredmeny = eredmeny(j_lapok, g_lapok)
+    if vart_eredmeny == kapott_eredmeny:
+        print("A játékos 21-nél kevesebb, a teszt sikeres")
+    else:
+        print("A játékos 21-nél kevesebb, a teszt megbukott")
+
+
+def jatekos_vesztett_tobbet_huzott_teszt():
+    jlapok = [10, 9, 1, 1]
+    glapok = [10, 9, 2]
+    vart_eredmeny = "A játékos vesztett!"
+    kapott_eredmeny = eredmeny(jlapok, glapok)
+    if vart_eredmeny == kapott_eredmeny:
+        print("A játékos többet húzott teszt sikeres")
+    else:
+        print("A játékos többet húzott teszt megbukott")
+
+def folyamatabra():
+#    jP = ossz(jL)
+#   gP = ossz(gL)
+
+#    jDb = lapDb(jL)
+#    gDb = lapDb(gL)
+    jP = 0
+    gP = 0
+    jDb = 0
+    gDb = 0
+
+    if jP <= 21 and gP <= 21:
+        if jP > 21:
+            print("Játékos vesztett")
+            exit()
+        elif gP > 21:
+            print("Gép vesztett")
+            exit()
+        else:
+            print("Döntetlen a ház nyert")
+            exit()
+
+        if jP > gP:
+            print("Játékos nyert")
+            exit()
+        elif gP > jP:
+            print("Gép nyert")
+            exit()
+        else:
+            if gP == jP:
+                if  jDb < gDb:
+                    print("Játékos nyert")
+                    exit()
+                elif jDb > gDb:
+                    print("Gép nyert")
+                    exit()
+                else:
+                    print("Döntetlen, osztoztok a nyereségen")
+                    exit()
